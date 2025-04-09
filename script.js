955f96316f561f72dffe3a4b378753efc3c97075
RISHAV SHRESTHA <shrestharishav81@gmail.com>
2025-04-09 21:32:47 +0300

    Added Contact me and Projects section, also pictures for Projects section

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ document.addEventListener("DOMContentLoaded", () => {
     const nav = document.getElementById("nav-links");
     nav.classList.toggle("show");
   }
+
+
+const images = [
+    'image/project.png',
+    'image/project2.png',
+    'image/project3.png',
+    'image/project4.png'
+  ];
+
+  var currentIndex = 0;
+  setInterval(cycleImages, 3000);
+  
+  function cycleImages() {
+    const imgElement = document.getElementById('project-img');
+    imgElement.src = images[currentIndex];
+    currentIndex = (currentIndex + 1) % images.length;
+  }
