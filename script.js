d1df9ebb11fcbffa41aab41332563c80ebeb36c7
RISHAV SHRESTHA <shrestharishav81@gmail.com>
2025-04-09 23:22:13 +0300

    Added Fixed Footer bar Added Typing effect in the  home section.

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+
 document.addEventListener("DOMContentLoaded", () => {
     const nav = document.querySelector('.navbar');
     const home = document.querySelector('.home');
+    const footer = document.querySelector('.footer');
     window.addEventListener('scroll', () => {
       nav.classList.toggle('scrolled', window.scrollY > 100);
       home.classList.toggle('scrolled', window.scrollY > 100);
+      footer.classList.toggle('scrolled', window.scrollY > 100);
     });
+    typeWriter();
   });
 
   function toggleMenu() {
@@ -21,10 +25,43 @@ const images = [
   ];
 
   var currentIndex = 0;
-  setInterval(cycleImages, 3000);
-  
+  setInterval(cycleImages, 100);
+
   function cycleImages() {
     const imgElement = document.getElementById('project-img');
     imgElement.src = images[currentIndex];
     currentIndex = (currentIndex + 1) % images.length;
+  }
+
+  var i = 0;
+  var txt = 'I am Rishav Shrestha';
+  var speed = 100;
+  
+  function type() {
+    if (i < txt.length) {
+      document.getElementById("hometitle").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(type, speed);
+    }
+    else
+    {
+    setTimeout(del, 500);
+    }
+  }
+  function del() {
+    if (i >4) {
+      document.getElementById("hometitle").innerHTML =document.getElementById("hometitle").innerHTML.substring(0, document.getElementById("hometitle").innerHTML.length - 1);
+      i--;
+      setTimeout(del, speed);
+    }
+    else
+    {
+    setTimeout(type, 500);
+    }
+  }
+
+  function typeWriter() {
+    i = 4;
+    document.getElementById("hometitle").innerHTML= "I am";
+    type();
   }
