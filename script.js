e2c2e35a72aadff2d593d283eb9339e1837a9676
RISHAV SHRESTHA <shrestharishav81@gmail.com>
2025-04-09 14:45:52 +0300

    Improved upon the scroll effect

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     const nav = document.querySelector('.navbar');
+    const home = document.querySelector('.home');
     window.addEventListener('scroll', () => {
       nav.classList.toggle('scrolled', window.scrollY > 100);
+      home.classList.toggle('scrolled', window.scrollY > 100);
     });
   });
+
+  function toggleMenu() {
+    const nav = document.getElementById("nav-links");
+    nav.classList.toggle("show");
+  }
